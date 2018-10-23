9c5e65a36841d024be1204d1af7052616d4a7c4d
Kover Cheng <18217249015@163.com>
2018-10-23 19:00:11 +0800

    MiBand 1A writeUserInfo complete get real time steps data & sensor data

--- app/src/main/java/cheng/kover/attacker/BLEDeviceAttacker.java
+++ app/src/main/java/cheng/kover/attacker/BLEDeviceAttacker.java
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
@@ -16,7 +17,7 @@ import java.util.UUID;
 
 public class BLEDeviceAttacker
 {
-	private static final String                TAG = "BLE Device Attacker";
+	private static final String                TAG = "DeviceAttacker";
 	private              Context               context;
 	private              BluetoothDevice       targetDevice;
 	private              BluetoothGatt         bluetoothGatt;
@@ -36,14 +37,14 @@ public class BLEDeviceAttacker
 
 				if (newState == BluetoothProfile.STATE_CONNECTED)
 				{
-					Log.i(TAG, "Connection established. Start discovering services.");
+					Log.i(TAG + "Callback", "Connection established. Start discovering services.");
 					gatt.discoverServices();
 				}
 				else
 				{
 					if (newState == BluetoothProfile.STATE_DISCONNECTED)
 					{
-						Log.w(TAG, "Disconnected!!!");
+						Log.w(TAG + "Callback", "Disconnected!!!");
 						gatt.close();
 					}
 				}
@@ -55,19 +56,19 @@ public class BLEDeviceAttacker
 				super.onServicesDiscovered(gatt, status);
 				if (status == BluetoothGatt.GATT_SUCCESS)
 				{
-					Log.i(TAG, "Services discovered.");
+					Log.i(TAG + "Callback", "Services discovered.");
 //					for (BluetoothGattService bluetoothGattService : gatt.getServices())
 //					{
-//						Log.d(TAG, " - Service: " + bluetoothGattService.getUuid());
+//						Log.d(TAG + "Callback", " - Service: " + bluetoothGattService.getUuid());
 //						for (BluetoothGattCharacteristic bluetoothGattCharacteristic : bluetoothGattService.getCharacteristics())
 //						{
-//							Log.d(TAG, " -- Characteristic: " + bluetoothGattCharacteristic.getUuid());
+//							Log.d(TAG + "Callback", " -- Characteristic: " + bluetoothGattCharacteristic.getUuid());
 //						}
 //					}
 				}
 				else
 				{
-					Log.e(TAG, "onServicesDiscovered received: " + status);
+					Log.e(TAG + "Callback", "onServicesDiscovered received: " + status);
 				}
 			}
 
@@ -86,14 +87,14 @@ public class BLEDeviceAttacker
 						{
 							firmwareVersion |= (value[12 + i] & 255) << i * 8;
 						}
-						Log.i(TAG, "Device Info: Firmware Version - " + firmwareVersion);
+						Log.i(TAG + "Callback", "Device Info: Firmware Version - " + firmwareVersion);
 					}
 					if (MiBandProfile.UUID_CHAR_DEVICE_NAME.equals(characteristic.getUuid()))
 					{
 						// Decode device name
 						try
 						{
-							Log.i(TAG, "Device Name" + Arrays.toString(characteristic.getValue()) + "," + new String(characteristic.getValue(), "UTF-8"));
+							Log.i(TAG + "Callback", "Device Name" + Arrays.toString(characteristic.getValue()) + "," + new String(characteristic.getValue(), "UTF-8"));
 						}
 						catch (UnsupportedEncodingException e)
 						{
@@ -103,7 +104,56 @@ public class BLEDeviceAttacker
 				}
 				else
 				{
-					Log.e(TAG, characteristic.getUuid() + "onServicesDiscovered received: " + status);
+					Log.e(TAG + "Callback", characteristic.getUuid() + "onServicesDiscovered received: " + status);
+				}
+			}
+
+			@Override
+			public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic)
+			{
+				super.onCharacteristicChanged(gatt, characteristic);
+
+				Log.d("test", "onCharacteristicChanged received: " + Arrays.toString(characteristic.getValue()));
+
+				// Receive characteristic notification data
+				if (MiBandProfile.UUID_CHAR_NOTIFICATION.equals(characteristic.getUuid()))
+				{
+					byte[] notificationValue = characteristic.getValue();
+
+					if (notificationValue.length != 1)
+					{
+						Log.e(TAG + "Callback", "Notification Char return wrong with " + notificationValue.length + " byte(s).");
+					}
+					else
+						switch (notificationValue[0])
+						{
+							case 0x6:
+								Log.e(TAG + "Callback", "Need to authenticate by tapping.");
+								break;
+							case 0x5: case 0xa: case 0x15:
+								Log.i(TAG + "Callback", "Successfully authenticated.");
+								break;
+							default:
+								Log.e(TAG + "Callback", "Unsolved message: " + notificationValue[0]);
+						}
+				}
+
+				// Receive real time steps data
+				if (MiBandProfile.UUID_CHAR_REALTIME_STEPS.equals(characteristic.getUuid()))
+				{
+					byte[] stepsData = characteristic.getValue();
+					if (stepsData.length == 4)
+					{
+						int steps = stepsData[3] << 24 | (stepsData[2] & 0xFF) << 16 | (stepsData[1] & 0xFF) << 8 | (stepsData[0] & 0xFF);
+						Log.i(TAG + "Callback", "Real time steps: " + steps);
+					}
+				}
+
+				// Receive sensor data
+				if (MiBandProfile.UUID_CHAR_SENSOR_DATA.equals(characteristic.getUuid()))
+				{
+					Log.i(TAG + "Callback", "Sensor data notification: " + Arrays.toString(characteristic.getValue()));
+					//TODO Decode sensor data
 				}
 			}
 		};
@@ -146,22 +196,44 @@ public class BLEDeviceAttacker
 					@Override
 					public void run()
 					{
-						// Pair with MiBand
-						readCharacteristic(MiBandProfile.UUID_SERVICE_MILI, MiBandProfile.UUID_CHAR_DATE_TIME);
 						delay();
+						setNotify(MiBandProfile.UUID_CHAR_NOTIFICATION);
+						delay();
+						// Pair with MiBand
 						writeCharacteristic(MiBandProfile.UUID_SERVICE_MILI, MiBandProfile.UUID_CHAR_PAIR, new byte[] {2});
 						delay();
+						// Check pair status
 						readCharacteristic(MiBandProfile.UUID_SERVICE_MILI, MiBandProfile.UUID_CHAR_PAIR);
 						delay();
+						// Read device info
 						readCharacteristic(MiBandProfile.UUID_SERVICE_MILI, MiBandProfile.UUID_CHAR_DEVICE_INFO);
 						delay();
+						// Read device name
 						readCharacteristic(MiBandProfile.UUID_SERVICE_MILI, MiBandProfile.UUID_CHAR_DEVICE_NAME);
 						delay();
+						// Write user info
 						MiBandUserInfo miBandUserInfo      = new MiBandUserInfo(0, 0, 175, 70, "mi_user_alias", 0);
 						byte[]         miBandUserInfoBytes = miBandUserInfo.toBytes(bluetoothDevice.getAddress());
 						Log.d(TAG, "Set User Info: " + miBandUserInfo.toString() + ", bytes: " + bytesToHex(miBandUserInfoBytes));
-// ***********************************Here's the write user info code.************************************************
 						writeCharacteristic(MiBandProfile.UUID_SERVICE_MILI, MiBandProfile.UUID_CHAR_USER_INFO, miBandUserInfoBytes);
+//						writeCharacteristic(MiBandProfile.UUID_SERVICE_MILI, MiBandProfile.UUID_CHAR_USER_INFO, new byte[]{15,5,13,2,2,9,8,7,6,5,0,10,1,13,8,2,0,5,10,11,8,2,11,14,11,9,3,8,5,9,12,15});
+						delay();
+						delay();
+						delay();
+						delay();
+						// Enable real time steps notify
+						writeCharacteristic(MiBandProfile.UUID_SERVICE_MILI, MiBandProfile.UUID_CHAR_REALTIME_STEPS, new byte[] {3, 1});
+						delay();
+						setNotify(MiBandProfile.UUID_CHAR_REALTIME_STEPS);
+						delay();
+						// Enable sensor notify
+						writeCharacteristic(MiBandProfile.UUID_SERVICE_MILI, MiBandProfile.UUID_CHAR_CONTROL_POINT, new byte[] {18, 1});
+						delay();
+						setNotify(MiBandProfile.UUID_CHAR_SENSOR_DATA);
+//						// Disable notify
+//						writeCharacteristic(MiBandProfile.UUID_SERVICE_MILI, MiBandProfile.UUID_CHAR_REALTIME_STEPS, new byte[] {3, 0});
+//						delay();
+//						writeCharacteristic(MiBandProfile.UUID_SERVICE_MILI, MiBandProfile.UUID_CHAR_CONTROL_POINT, new byte[] {12, 0});
 					}
 				}, 5000);
 			}
@@ -183,20 +255,34 @@ public class BLEDeviceAttacker
 
 	public void writeCharacteristic(UUID serviceUUID, UUID characteristicUUID, byte[] value)
 	{
-		Log.d("test", "service: " + serviceUUID + ", char: " + characteristicUUID + ", write v: " + Arrays.toString(value));
 		BluetoothGattCharacteristic bluetoothGattCharacteristic = bluetoothGatt.getService(serviceUUID).getCharacteristic(characteristicUUID);
 		if (null == bluetoothGattCharacteristic)
 		{
-			Log.e(TAG, "Characteristic " + characteristicUUID + " not exist.");
+			Log.e(TAG + "IO", "Characteristic " + characteristicUUID + " not exist.");
 			return;
 		}
 		bluetoothGattCharacteristic.setValue(value);
 		if (! bluetoothGatt.writeCharacteristic(bluetoothGattCharacteristic))
 		{
-			Log.e(TAG, "Bluetooth GATT write characteristic" + characteristicUUID + " failed.");
+			Log.e(TAG + "IO", "Bluetooth GATT write characteristic " + characteristicUUID + " failed.");
 			return;
 		}
-		Log.i(TAG, "Characteristic" + characteristicUUID + " successfully written.");
+		Log.i(TAG + "IO", "Characteristic" + characteristicUUID + " successfully written value: " + Arrays.toString(value));
+	}
+
+	public void setNotify(UUID characteristicUUID)
+	{
+		Log.d(TAG + "IO", "Set char " + characteristicUUID + " Notify.");
+		BluetoothGattCharacteristic bluetoothGattCharacteristic = bluetoothGatt.getService(MiBandProfile.UUID_SERVICE_MILI).getCharacteristic(characteristicUUID);
+		if (null == bluetoothGattCharacteristic)
+		{
+			Log.e(TAG + "IO", "Characteristic " + characteristicUUID + " not exist.");
+			return;
+		}
+		bluetoothGatt.setCharacteristicNotification(bluetoothGattCharacteristic, true);
+		BluetoothGattDescriptor bluetoothGattDescriptor = bluetoothGattCharacteristic.getDescriptor(MiBandProfile.UUID_DESCRIPTOR_UPDATE_NOTIFICATION);
+		bluetoothGattDescriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+		bluetoothGatt.writeDescriptor(bluetoothGattDescriptor);
 	}
 
 	public void readCharacteristic(UUID serviceUUID, UUID characteristicUUID)
@@ -204,19 +290,8 @@ public class BLEDeviceAttacker
 		BluetoothGattCharacteristic bluetoothGattCharacteristic = bluetoothGatt.getService(serviceUUID).getCharacteristic(characteristicUUID);
 		if (! bluetoothGatt.readCharacteristic(bluetoothGattCharacteristic))
 		{
-			Log.e(TAG, "Bluetooth GATT read characteristic " + characteristicUUID + " failed.");
+			Log.e(TAG + "IO", "Bluetooth GATT read characteristic " + characteristicUUID + " failed.");
 			return;
 		}
 	}
 }
-
-/*
-TODO				"setNormalNotifyListener",11
-TODO				"setRealtimeStepsNotifyListener",12
-TODO				"enableRealtimeStepsNotify",13
-TODO				"disableRealtimeStepsNotify",14
-TODO				"miband.setLedColor(LedColor.RED);",17
-TODO				"setSensorDataNotifyListener",19
-TODO				"enableSensorDataNotify",20
-TODO				"disableSensorDataNotify",21
-*/
